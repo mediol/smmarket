620947fadc77673be5e39f0347cdec7d6173bca1
mediol <bearjonas@ukr.net>
2021-12-02 09:23:45 +0200

    first commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+$('.js-open-modal').click(function(event) {
+    event.preventDefault();
+    
+    var modalName = $(this).attr('data-modal');
+    var modal = $('.js-modal[data-modal="' + modalName + '"]');
+    
+    modal.addClass('is-show');
+    $('.js-modal-overlay').addClass('is-show')
+ });
+ 
+ $('.js-modal-close').click(function() {
+    $(this).parent('.js-modal').removeClass('is-show');
+    $('.js-modal-overlay').removeClass('is-show');
+ });
+    
+ $('.js-modal-overlay').click(function() {
+    $('.js-modal.is-show').removeClass('is-show');
+    $(this).removeClass('is-show');
+ }) 
+
+ document.body.addEventListener('keyup', function (e) {
+    var key = e.keyCode;
+
+    if (key == 27) {
+        document.querySelector('.modal.active').classList.remove('active');
+        document.querySelector('.overlay.active').classList.remove('active');
+    };
+}, false);
